0fd49c404ebd49b42dfc5d4cef11a4d1fd2fc76e
Chris Brown <chris.matt.brown@gmail.com>
2021-10-11 10:26:43 -0400

    Initial Commit

---
+++ Day 1 - Band Name/Band_Name_Generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+city_name = input("What City did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your Band Name could be " + city_name + " " + pet_name)
